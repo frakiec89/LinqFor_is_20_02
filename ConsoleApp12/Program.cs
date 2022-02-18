ea033deb92f25240e0e7c26a584569b8138dbc92
frakiec89 <45938852+frakiec89@users.noreply.github.com>
2022-02-18 11:36:58 +0400

    Добавьте файлы проекта.

---
+++ ConsoleApp12/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Linq;
+namespace ConsoleApp12
+{
+    internal class Program 
+    {
+        static void Main(string[] args)
+        {
+         UserController controller = new UserController();
+
+            Console.WriteLine("Пользователи старше 18 лет:");
+
+            controller.Users.Where (x=> x.Age >= 18).
+                OrderBy(x=>x.Name).
+                ToList().
+                ForEach (x => Console.WriteLine (x));
+
+        }
+    }
+
+
+    public class User
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+
+        public override string ToString()
+        {
+            return $"Имя -  {Name}, Возраст - {Age}";
+        }
+
+    }
+}
---
+++ ConsoleApp12/User.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ConsoleApp12
+{
+    public class UserController
+    {
+        Random random = new Random(); 
+        public List<User> Users { get; set; }
+
+        public  UserController ()
+        {
+            Users = new List<User>()
+            {
+                new User { Name ="иван" , 
+                    Age= random.Next
+                (16,22)
+                },
+
+                  new User { Name ="Денис" ,
+                    Age= random.Next
+                (16,22)
+                },
+
+                    new User { Name ="Лена" ,
+                    Age= random.Next
+                (16,22)
+                },
+
+                      new User { Name ="Петя" ,
+                    Age= random.Next
+                (16,22)
+                },
+            };
+        }
+    }
+}
